888567a2265de3ad6820407a5027929d89bd6d23
sherin muhter <sherinmuhter@gmail.com>
2019-02-18 15:49:22 -0800

    new project for git config

---
+++ Uginish/src/Brinji_Qedem/Section_01.java
@@ -0,0 +1,30 @@
+package Brinji_Qedem;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Section_01 {
+	public static void main(String[] arg) {
+		
+		// 1st java pup-up
+		System.out.println("Selenium is not a Tool! Just a Jar File! ");
+		
+		
+		// Test in the different browser: Chrome, Firefox
+		// 1st step : set property 
+		System.setProperty("webdriver.chrome.driver", "/Users/sherinmuhtar/Desktop/Selenium Dependencies/Drivers/chromedriver");
+		//System.setProperty("webdriver.gecko.driver", "/Users/sherinmuhtar/Desktop/Selenium Dependencies/Drivers/geckodriver");
+		
+		// create object for driver class
+		WebDriver driver1 = new ChromeDriver();
+		WebDriver driver2 =  new FirefoxDriver();
+		driver1.get("http://youtube.com");
+		driver2.get("http://amazon.com");
+		
+		System.out.println(driver1.getTitle());
+		System.out.println(driver2.getTitle());
+		
+
+	}
+}
