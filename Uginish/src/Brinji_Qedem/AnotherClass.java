983c679945b375a0c667cc74386cc9bb5801338d
Shukur <sukuri33@gmail.com>
2019-02-18 16:08:35 -0800

    Added one more class

---
+++ Uginish/src/Brinji_Qedem/AnotherClass.java
@@ -0,0 +1,10 @@
+package Brinji_Qedem;
+
+public class AnotherClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("Added class");
+	}
+
+}
